14ccd9cdf13bac7f4bd47a490f965c339a03290c
Calum Mackervoy <c.mackervoy@gmail.com>
2023-01-28 17:11:38 +0100

    bump rdflib version

--- lit/mud.py
+++ lit/mud.py
@@ -8,14 +8,10 @@ TODO: this should belong in it's own library and be published to PyPi for reuse
 MUD_BASE_URL = "https://raw.githubusercontent.com/Multi-User-Domain/vocab/main/mud.ttl"
 MUD_ACCT_BASE_URL = "https://raw.githubusercontent.com/Multi-User-Domain/vocab/main/mudacct.ttl"
 
-# NOTE: when upgrading to Python 3.11, this class can be replaced with the built-in class StrEnum
-class StrEnum(str, Enum):
-    def __str__(self) -> str:
-        return self.value
-
-class MUD(StrEnum):
+class MUD:
     pass
 
-class MUD_ACCT(StrEnum):
+
+class MUD_ACCT:
     Account = MUD_ACCT_BASE_URL + "#Account",
-    charactersList = MUD_ACCT_BASE_URL + "#CharacterList",
+    charactersList = MUD_ACCT_BASE_URL + "#CharacterList"
