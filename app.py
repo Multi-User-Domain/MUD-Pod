566acd5ea765ca00160159fd13c0b3236c24860d
Calum Mackervoy <c.mackervoy@gmail.com>
2023-01-28 17:32:32 +0100

    simple GET user

--- app.py
+++ app.py
@@ -16,6 +16,13 @@ db = client.mud
 def main():
     return None, 204
 
+# TODO: replace user_id with webid
+@app.route("/user/<int:user_id>/", methods=['GET'])
+def get_user(user_id):
+    result = db.user.find_one({"id": user_id})
+    json_result = json.loads(json_util.dumps(result))
+    return jsonify(json_result), 200, {'Content-Type': 'application/ld+json'}
+
 @app.route("/user/", methods=['POST'])
 def create_user():
     body = request.get_json()
@@ -24,8 +31,16 @@ def create_user():
 
     webid = f"http://localhost:5000/user/{str(user_id)}/"
     user_data = {
-        prop: body["id"], # TODO: replace with webid, not intended as part of the implementation
-        MUD_ACCT.charactersList: [],
+        "@context": {
+            "id": {
+                "@id": "https://raw.githubusercontent.com/Multi-User-Domain/vocab/main/mud.ttl#id"
+            },
+            "charactersList": {
+                "@id": MUD_ACCT.charactersList
+            }
+        },
+        "id": body["id"], # TODO: replace with webid, not intended as part of the implementation
+        "charactersList": [],
         "@id": webid,
         "@type": MUD_ACCT.Account
     }
